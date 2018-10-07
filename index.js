1496a0fcd240451a644e8d1f551bca0b4ef59723
Nathan M <nathanminissale@gmail.com>
2018-10-07 07:41:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,11 +15,8 @@ function increaseRankBy(n) {
   rankedLists.forEach(function(item, index, array) {
     let children = item.children
     for (var i = 0; i < children.length; i++) {
-      // console.log(`parseInt on children.inner:\n${parseInt(children[i].innerHTML) + n}`);
       let oldValue = parseInt(children[i].innerHTML);
       let newValue = oldValue + n;
-      // console.log(`oldValue: ${oldValue} \nnewValue: ${newValue}`);
-      // console.log(typeof children);
       children[i].innerHTML = newValue;
     }
   });
